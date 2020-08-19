593d7b9b9d03e47bcc16fdc08266422e2fdaf4c7
Il Harper <afanyiyu@hotmail.com>
2020-08-19 14:53:09 +0800

    feat: Blend Function Update

--- SubLight.Classic/SubLight.Classic.Blend.cpp
+++ SubLight.Classic/SubLight.Classic.Blend.cpp
@@ -5,11 +5,6 @@
 #define _b(c) (((c)>>8)&0xFF)
 #define _a(c) ((c)&0xFF)
 
-PF_Pixel* GetPixel(PF_EffectWorld* def, int x, int y)
-{
-	return reinterpret_cast<PF_Pixel*>(reinterpret_cast<char*>(def->data) + y * def->rowbytes + x * sizeof(PF_Pixel));
-}
-
 void BlendSingle(PF_EffectWorld* def, int dst_stride, int w, int h, int color, unsigned char* src, int src_stride, int dst_x, int dst_y, int src_w, int src_h)
 {
 	if (!def || !src) return;
@@ -22,44 +17,25 @@ void BlendSingle(PF_EffectWorld* def, int dst_stride, int w, int h, int color, u
 	src_w = MIN(w - dst_x, src_w);
 	src_h = MIN(h - dst_y, src_h);
 
-	//unsigned int* dstp = reinterpret_cast<unsigned*>(dst) + size_t(dst_y) * (dst_stride >> 2) + dst_x;
-	
-	PF_Pixel* dstp = GetPixel(def, dst_x, dst_y);
+	PF_Pixel* dstp = reinterpret_cast<PF_Pixel*>(reinterpret_cast<char*>(def->data) + dst_y * def->rowbytes + dst_x * sizeof(PF_Pixel));
 	if (!dstp) return;
 	
 	unsigned char* srcp = reinterpret_cast<unsigned char*>(src);
 
 	int src_stride2 = src_stride - src_w;
-	int dst_stride2 = (dst_stride >> 2) - src_w;
+	int dst_stride2 = dst_stride / 4 - src_w;
 
-	int x = 0, y = 0;
-
-	while (y < src_h)
+	for (int y = 0;  y < src_h; y++, dstp += dst_stride2, srcp += src_stride2)
 	{
-		while (x < src_w)
+		for (int x = 0; x < src_w; x++, dstp++, srcp++)
 		{
 			unsigned int k = unsigned(*srcp) * opacity / 0xFF;
 			unsigned int ck = 255 - k;
 
-			unsigned char finr = (k * r + ck * dstp->red) / 0xFF;
-			unsigned char fing = (k * g + ck * dstp->green) / 0xFF;
-			unsigned char finb = (k * b + ck * dstp->blue) / 0xFF;
-			unsigned char fina = 0xFF - ck * (0xFF - dstp->alpha) / 0xFF;
-			
-			//*dstp = (((fina & 0xFF) << 24) + ((finb & 0xFF) << 16) + ((fing & 0xFF) << 8) + (finr & 0xFF));
-
-			dstp->red = finr;
-			dstp->green = fing;
-			dstp->blue = finb;
-			dstp->alpha = fina;
-
-			x++;
-			dstp++;
-			srcp++;
+			dstp->red = (k * r + ck * dstp->red) / 255;
+			dstp->green = (k * g + ck * dstp->green) / 255;
+			dstp->blue = (k * b + ck * dstp->blue) / 255;
+			dstp->alpha = 0xFF - ck * (0xFF - dstp->alpha) / 255;
 		}
-
-		y++;
-		dstp += dst_stride2;
-		srcp += src_stride2;
 	}
 }
